4e7ed8495a32b15fd576c9175dbfa025330132e4
Ksinia <xiatian@mail.ru>
2019-12-06 11:20:37 +0100

    Create image model

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const app = express();
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 const db = require("./db");
+const Image = require("./image/model");
